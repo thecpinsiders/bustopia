951bf16bc3384ef2865eb3ec69a227f3c3873a93
thecpinsiders <epicbattle85@gmail.com>
2021-01-10 23:10:12 +0800

    completed search bus stop by 5 digit number

--- routes.js
+++ routes.js
@@ -50,17 +50,21 @@ var routes = function () {
     router.get('/', function (req, res) {
         res.sendFile(__dirname + "/views/index.html");
     });
-
-    router.get('/edit', function (req, res) {
-        res.sendFile(__dirname + "/views/editEvent.html");
-    });
+    // router.get('/edit', function (req, res) {
+    //     res.sendFile(__dirname + "/views/editEvent.html");
+    // });
     router.get('/register', function (req, res) {
         res.sendFile(__dirname + "/views/register.html");
     });
     router.get('/login', function (req, res) {
         res.sendFile(__dirname + "/views/login.html");
     });
-
+    router.get('/favorites', function (req, res) {
+        res.sendFile(__dirname + "/views/favorites.html");
+    });
+    router.get('/results', function (req, res) {
+        res.sendFile(__dirname + "/views/results.html");
+    });
     router.get('/css/*', function(req, res)  {
         res.sendFile(__dirname+"/views/"+req.originalUrl);
     });
@@ -69,67 +73,67 @@ var routes = function () {
         res.sendFile(__dirname+"/views/"+req.originalUrl);
     });
 
-    router.get('/events', function (req, res) {
-        db.getAllEvents(function (err, events) {
-            if (err) {
-                res.status(500).send("Unable to get all events.");
-            } else {
-                res.status(200).send(events);
-            }
-        })
-    })
-    router.get('/events/:id', function (req, res) {
-        var id = req.params.id;
-        db.getEvent(id, function (err, event) {
-            if (err) {
-                res.status(500).send("Unable to find an event with this id");
-            } else {
-                res.status(200).send(event);
-            }
-        })
-    })
-    router.post('/events', function (req, res) {
-        var data = req.body;
-        db.addEvent(data.name, data.description, data.startDate, data.startTime, data.endDate, data.endTime,
-            function (err, event) {
-                if (err) {
-                    res.status(500).send("Unable to add a new event");
-                } else {
-                    res.status(200).send("Event has been successfully added!");
-                }
-            })
-    });
+    // router.get('/events', function (req, res) {
+    //     db.getAllEvents(function (err, events) {
+    //         if (err) {
+    //             res.status(500).send("Unable to get all events.");
+    //         } else {
+    //             res.status(200).send(events);
+    //         }
+    //     })
+    // })
+    // router.get('/events/:id', function (req, res) {
+    //     var id = req.params.id;
+    //     db.getEvent(id, function (err, event) {
+    //         if (err) {
+    //             res.status(500).send("Unable to find an event with this id");
+    //         } else {
+    //             res.status(200).send(event);
+    //         }
+    //     })
+    // })
+    // router.post('/events', function (req, res) {
+    //     var data = req.body;
+    //     db.addEvent(data.name, data.description, data.startDate, data.startTime, data.endDate, data.endTime,
+    //         function (err, event) {
+    //             if (err) {
+    //                 res.status(500).send("Unable to add a new event");
+    //             } else {
+    //                 res.status(200).send("Event has been successfully added!");
+    //             }
+    //         })
+    // });
 
-    router.put('/events', function (req, res) {
-        var data = req.body;
-        db.updateEvent(data.id, data.name, data.description, data.startDate, data.startTime, data.endDate, data.endTime,
-            function (err, event) {
-                if (err) {
-                    res.status(500).send("Unable to update the event");
-                } else {
-                    if (event == null) {
-                        res.status(200).send("No event is updated");
-                    } else {
-                        res.status(200).send("Event has been updated successfully");
-                    }
-                }
-            });
-    })
+    // router.put('/events', function (req, res) {
+    //     var data = req.body;
+    //     db.updateEvent(data.id, data.name, data.description, data.startDate, data.startTime, data.endDate, data.endTime,
+    //         function (err, event) {
+    //             if (err) {
+    //                 res.status(500).send("Unable to update the event");
+    //             } else {
+    //                 if (event == null) {
+    //                     res.status(200).send("No event is updated");
+    //                 } else {
+    //                     res.status(200).send("Event has been updated successfully");
+    //                 }
+    //             }
+    //         });
+    // })
 
-    router.delete('/events/:id', function (req, res) {
-        var id = req.params.id;
-        db.deleteEvent(id, function (err, event) {
-            if (err) {
-                res.status(500).send("Unable to delete the event");
-            } else {
-                if (event == null) {
-                    res.status(200).send("No event is deleted");
-                } else {
-                    res.status(200).send("Event has been deleted successfully");
-                }
-            }
-        });
-    })
+    // router.delete('/events/:id', function (req, res) {
+    //     var id = req.params.id;
+    //     db.deleteEvent(id, function (err, event) {
+    //         if (err) {
+    //             res.status(500).send("Unable to delete the event");
+    //         } else {
+    //             if (event == null) {
+    //                 res.status(200).send("No event is deleted");
+    //             } else {
+    //                 res.status(200).send("Event has been deleted successfully");
+    //             }
+    //         }
+    //     });
+    // })
 
     router.post('/register', function (req, res) {
         var data = req.body;
@@ -161,6 +165,7 @@ var routes = function () {
         })
     })
 
+
     router.get("/logout", function (req, res) {
         var token = req.query.token;
         if (token == undefined) {
@@ -178,7 +183,41 @@ var routes = function () {
         }
     })
 
-    router.get('/getbusarrival', function (req, res) {
+    //Set the bus services of choice as favourite
+    router.put('/api/savefavouritebus', function(req,res){
+        
+    })
+
+    //Set the bus stop of choice as favourite
+    router.put('/api/savefavouritebustop', function(req,res){
+
+    })
+
+    //Search bus route via bus number
+    router.post('/api/searchbusroute', function (req, res) {
+        
+    })
+    //Search for Bus Services by bus stop name e.g Orchard MRT Station
+    router.post('/api/searchstopname', function (req, res) {
+        
+    })
+    //Search for Bus stops via bus stop number
+    router.post('/api/searchstopnumber', function (req, res) {
+        
+    })
+
+    //View Bus Service information like First bus,Last bus and what operator operates that bus service
+    router.get('/api/getserviceinfo', function (req, res) {
+        
+    })
+
+    //Search for Bus Route via service number e.g 190.
+    router.get('/api/getserviceroute', function (req, res) {
+        
+    })
+
+
+    router.get('/api/getbusarrival', function (req, res) {
         var data = req.body;
         db.getData(data.busStop, function(err,service){
             if (err) {
@@ -190,6 +229,31 @@ var routes = function () {
             }
         })
     })
+
+    router.post('/getbusarrival', function (req, res) {
+        var data = req.body;
+        db.getData(data.BusStopCode, function(err,service){
+            if (err) {
+                //console.log(service);
+                console.log(data.service);
+                res.status(500).send("Unable to get bus information");
+            } else {
+                res.status(200).send(service);
+            }
+        })
+    })
+
+    // router.get('/getbusarrival/?=:busstop', function (req, res) {
+    //         var data = req.params.busstop;
+    //         db.getData(data, function (err, service) {
+    //             if (err) {
+    //                 console.log(data.service);
+    //                 res.status(500).send("Unable to get bus information");
+    //             } else {
+    //                 res.status(200).send(service);
+    //             }
+    //         })
+    //     })
 
     return router;
 };
